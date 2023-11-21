e1963cdc12c61f91fefa8947cc752b40cac508f1
dehzi <khyle.chua321@gmail.com>
2023-11-21 12:11:06 +0800

    init

---
+++ scripts/home.js
@@ -0,0 +1,5 @@
+// JavaScript to handle event carousel or any interactive elements
+document.addEventListener('DOMContentLoaded', function() {
+  // Initialization code for the events carousel
+  // For example, you could use a library like Slick or Swiper for the carousel.
+});
